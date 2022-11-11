ec5396e3d4f1ff7e504d1345644afa4bfac37dd7
Nick Griffey <nick.griffey@protonmail.com>
2022-11-11 16:55:39 -0500

    Rewrite of previous functions

---
+++ js/class-info.js
@@ -0,0 +1,78 @@
+initializePage();
+
+function initializePage() {
+  // Get current page name and store in local storage
+  let path = window.location.pathname;
+  let page = "";
+  if (path == "/") {
+    page = "index.html";
+  } else {
+    page = path.split("/").pop();
+  }
+
+  if (page == "index.html") {
+    sessionStorage.setItem("selectedClass", null);
+  } else if (sessionStorage.getItem("selectedClass") !== null) {
+    loadClass();
+  }
+	displayClassHome();
+}
+
+async function displayClassModule(module_num) {
+  // Function to retrieve specified class module info and load it into template
+	module_num_str = String(module_num)
+	if (module_num_str.length === 1) {
+		module_num_str = "0" + module_num_str
+	}
+
+	className = sessionStorage.getItem("selectedClass");
+	path = "course-data/" + className.toLowerCase().replace(" ", "_") + "/course_info/pages/" + module_num_str + ".html";
+	page = await fetch(path);
+
+  // Read in correct class module
+  info_html = await page.text();
+
+  // Put info into main content section of template
+  document.getElementById("main-content").innerHTML = info_html;
+}
+
+/*
+ * Use after clicking on a nav tab to set the new tab to active and make the
+ * others inactive
+ * TODO: This might need to be modified to also reset modules tab
+ */
+function resetActiveNav(activeNav) {
+	ul = document.getElementById(activeNav).parentElement.parentElement;
+	lis = ul.children;
+	for (var i = 0; i < lis.length; i++) {
+		span = lis[i].children[0];
+		span.classList.remove("active");
+	}
+
+	document.getElementById(activeNav).classList.add("active");
+}
+
+async function displayClassHome() {
+	home = await fetch("class-home.html");
+	html = await home.text();
+
+	document.getElementById("main-content").innerHTML = html;
+	resetActiveNav("nav-home");
+}
+
+async function displayClassSyllabus() {
+	className = sessionStorage.getItem("selectedClass");
+	path = "course-data/" + className.toLowerCase().replace(" ", "_") + "/course_info/syllabus.html";
+
+	syllabus = await fetch(path);
+	html = await syllabus.text();
+
+	document.getElementById("main-content").innerHTML = html;
+	resetActiveNav("nav-syllabus");
+}
+
+function loadClass() {
+  className = sessionStorage.getItem("selectedClass");
+
+  document.getElementById("class_name").innerHTML = className;
+}
